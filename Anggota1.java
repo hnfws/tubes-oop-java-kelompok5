240f3a3a08ad30a8dfd609cf633ba279f1cca84b
hnfws <muhanafawwas829@gmail.com>
2025-12-23 19:00:26 +0700

    perkenalan

---
+++ Anggota1.java
@@ -0,0 +1,11 @@
+public class Anggota1 {
+    public static void main(String[] args) {
+        String nama = "Muhana Fawwas Sausan";
+        String nim = "1324073";
+        String jurusan = "Sistem Informasi Industri Otomotif";
+
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("Jurusan: " + jurusan);
+    }
+}
